8dd337d30785a68cfc3d068dca66fc5a2f1dface
hfgbarrigas <hfgbarrigas@gmail.com>
2024-04-12 15:29:06 +0100

    feat: support logs exporting

--- opentelemetry-user-events-logs/src/logs/reentrant_logprocessor.rs
+++ opentelemetry-user-events-logs/src/logs/reentrant_logprocessor.rs
@@ -1,3 +1,4 @@
+use std::any::Any;
 use std::fmt::Debug;
 
 use opentelemetry::logs::LogResult;
@@ -59,7 +60,11 @@ impl opentelemetry_sdk::logs::LogProcessor for ReentrantLogProcessor {
         self.event_exporter.event_enabled(level, target, name)
     }
 
+<<<<<<< HEAD
     fn as_any(&self) -> &dyn std::any::Any {
+=======
+    fn as_any(&self) -> &dyn Any {
+>>>>>>> 5941cb9 (feat: support logs exporting)
         self
     }
 }
